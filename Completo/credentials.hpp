22bd8c0617ce3b5f582fe3fdc69f545cd494262e
KeevinSanchez <105021360+KeevinSanchez@users.noreply.github.com>
2023-06-23 11:35:21 -0300

    Add files via upload

---
+++ Completo/credentials.hpp
@@ -0,0 +1,22 @@
+// Credenciales Red WiFi
+#define WIFI_SSID "Estudiantes"
+#define WIFI_PASSWORD "educar_2018"
+// Credenciales Adafruit
+#define ADAFRUIT_USER "keevinsanchez"
+#define ADAFRUIT_KEY "aio_QlHw17FQSXpIEoQaa0Vpct2Wba19"
+// Publicar
+#define ADAFRUIT_FEED_LED ADAFRUIT_USER "keevinsanchez/feeds/led"
+// Suscripción
+#define ADAFRUIT_FEED_ON_OFF ADAFRUIT_USER "keevinsanchez/feeds/led-on-off"
+// Servidor
+#define ADAFRUIT_SERVER "io.adafruit.com"
+#define ADAFRUIT_PORT 1883
+
+char ADAFRUIT_ID[30];
+
+#define LED_1   27
+
+
+float t = 0;
+float h = 0;
+
